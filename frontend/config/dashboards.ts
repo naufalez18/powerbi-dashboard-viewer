41c2f03d640b90d976699a5a8aff0937c7a4330a
Leap <git@leap.new>
2025-08-07 17:43:13 +0000

    Manual code change

--- frontend/config/dashboards.ts
+++ frontend/config/dashboards.ts
@@ -9,17 +9,17 @@ export const powerBiDashboards: Dashboard[] = [
   {
     id: 'dashboard-1',
     name: 'Sales Dashboard',
-    url: 'https://app.powerbi.com/view?r=eyJrIjoiYWJjZGVmZ2gtaWprbC1tbm9wLXFyc3QtdXZ3eHl6MTIzNCIsInQiOiJjMDFhMmIzYy00ZDVlLTZmN2ctOGg5aS0wajFrMmwzbTRuNW8ifQ%3D%3D',
+    url: 'https://app.powerbi.com/view?r=eyJrIjoiNTFkN2Q4ODEtMzVjZi00YTZlLWI5MDEtZjZkOTE5NjY3ODgwIiwidCI6IjRiZGJiZTdkLTYzZGYtNGU1Yy05NTNlLTkyODYzMTY4OTVlNiIsImMiOjEwfQ%3D%3D',
   },
   {
     id: 'dashboard-2',
     name: 'Marketing Dashboard',
-    url: 'https://app.powerbi.com/view?r=eyJrIjoiZGVmZ2hpams0LWxtbm8tcHFycy10dXZ3LXh5ejEyMzQ1IiwidCI6ImMwMWEyYjNjLTRkNWUtNmY3Yy04aDlpLTBqMWsybDNtNG41byJ9',
+    url: 'https://app.powerbi.com/view?r=eyJrIjoiN2U5YThjNGYtOGFjMi00M2FjLWI4OWUtNDYzNjNhOTMzZGQ5IiwidCI6IjRiZGJiZTdkLTYzZGYtNGU1Yy05NTNlLTkyODYzMTY4OTVlNiIsImMiOjEwfQ%3D%3D&pageName=5d09de0a920dc3d03aa6',
   },
   {
     id: 'dashboard-3',
     name: 'Financial Dashboard',
-    url: 'https://app.powerbi.com/view?r=eyJrIjoiZ2hpamtsbW4tb3Bxci1zdHV2LXd4eXotMTIzNDU2IiwidCI6ImMwMWEyYjNjLTRkNWUtNmY3Yy04aDlpLTBqMWsybDNtNG41byJ9',
+    url: 'https://app.powerbi.com/view?r=eyJrIjoiMzZhYmJmODMtMDRhNC00OTMwLWI0OWYtNTc5OWIzY2I1Yjc1IiwidCI6IjRiZGJiZTdkLTYzZGYtNGU1Yy05NTNlLTkyODYzMTY4OTVlNiIsImMiOjEwfQ%3D%3D',
   },
   {
     id: 'dashboard-4',
